71450c03fefc540293bacff99e1b28f66b38f481
Reese Wilson <reese@shinymayhem.com>
2015-03-15 15:55:14 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Sent GET request to this server to get a randomly generated token
---
+++ fetch-repo.sh
@@ -0,0 +1,4 @@
+git init
+git remote add -t master origin http://github.com/shinymayhem/micro-token-gen
+git fetch
+git merge origin/master
